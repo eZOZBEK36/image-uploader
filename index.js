f51fa27ffe927ce2e8060e3c289a9ddc5ab0af3e
E'zozbek <ezozbekrasulov789@gmail.com>
2022-01-14 17:07:15 +0500

    deploy

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT ?? 5000
 
 app.use(async (req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*')
-	res.header('Access-Control-Allow-Methods', 'GET, POST')
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
 	res.header('Access-Control-Allow-Headers', 'Content-Type')
 	next()
 })
